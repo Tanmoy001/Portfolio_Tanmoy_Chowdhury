06798181e57fdfbcaa4a77423ba9196a3866a03c
Tanmoy001 <chowdhurytanmoy580@gmail.com>
2022-10-31 17:13:38 +0530

    updated3110

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import './Experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+     <h5>What skills I have</h5>
+     <h2>My Experience</h2>
+     <div className='container experience_container'>
+        <div className='experience_fontend'>
+              <h3>Web Development</h3>
+              <div className='experience_content'>
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>HTML</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>CSS</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>JS</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>React</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>NodeJs</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>MongoDB</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              </div>
+        </div>
+
+        <div className='experience_fontend'>
+              <h3>Artificial intelligence</h3>
+              <div className='experience_content'>
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>PANDAS</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>NUMPAY</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>scikit-learn</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+                    <div className='experience_details'>
+                      <BsFillPatchCheckFill className='icon'/>
+                      <div><h4>reinforcement learning</h4>
+                      <small className='experience_backend'>Experienced</small></div>
+                    </div>
+              
+              </div>
+        </div>
+     </div>
     </section>
   )
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './Portfolio.css'
+import img1 from '../../assets/Image1.jpeg'
 const Portfolio = () => {
-  return (
+  
+    const data=[
+      {id:1,
+      image:img1,
+      title:'title',
+      github:'https://github.com/Tanmoy001/Text-Modifier',
+      demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+    },
+    {id:2,
+      image:img1,
+      title:'title',
+      github:'https://github.com/Tanmoy001/Text-Modifier',
+      demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+    },
+    {id:3,
+      image:img1,
+      title:'title',
+      github:'https://github.com/Tanmoy001/Text-Modifier',
+      demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+    }
+    ]
+    return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+      <div className='container protfolio_container'>
+        { data.map(({id, image, title, github, demo})=>{
+          return(
+        <div key={id}className='portfolio_item'>
+          <div className='portfolio_item_img'>
+            <img src={image}alt={title}/>
+          </div>
+          <h3>{title}</h3>
+          <div className='portfolio_item-cta'>
+            <a href={github} className='btn'>Github</a>
+            <a href={demo} className='btn btn-primary'>Live Demo</a>
+          </div>
+        </div>
+          )
+      })}
+        </div>
+      
     </section>
   )
 }
