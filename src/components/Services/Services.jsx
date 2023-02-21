f778710360b078e58069b32383ec9fc78e4bfefa
Tanmoy001 <chowdhurytanmoy580@gmail.com>
2023-02-22 02:41:03 +0530

    p21

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './Services.css'
+import pic from '../../assets/3242120.png';
 const Services = () => {
   return (
     <section id='services'>
-      services
+        <h5>What skills I have</h5>
+         <h2>My Experience</h2>
+      <div className="container">
+        <div className="card">
+            <div className="lines"></div>
+            <div className="imgBx">
+            <img src={pic} alt='pic'/>
+            </div>
+            <div className="content">
+            <div className="details">
+                <h2>Design</h2>
+                <p>sadfdsagfhdsgfjk</p>
+            </div>
+        </div>
+        </div>
+        <div className="card">
+            <div className="lines"></div>
+            <div className="imgBx">
+                <img src={pic} alt='pic'/>
+            </div>
+            <div className="content">
+            <div className="details">
+                <h2>Design</h2>
+                <p>sadfdsagfhdsgfjk</p>
+            </div>
+        </div>
+        </div>
+        <div className="card">
+            <div className="lines"></div>
+            <div className="imgBx">
+            <img src={pic} alt='pic'/>
+            </div>
+            <div className="content">
+            <div className="details">
+                <h2>Design</h2>
+                <p>sadfdsagfhdsgfjk</p>
+            </div>
+        </div>
+        </div>
+    </div>
     </section>
   )
 }
