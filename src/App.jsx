f9ffe12cb244c9d11f5e9db1a19dc1aa394c2c2d
Tanmoy001 <chowdhurytanmoy580@gmail.com>
2023-03-10 02:31:29 +0530

    final_loader_updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@ import Services from './components/Services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import React,{useState,useEffect} from 'react';
+import BeatLoader from 'react-spinners/BeatLoader'
 
 
 const App = ()=>{
-      const setTheme =(color)=>{
+      const [loader,setLoader]=useState(false)
+      useEffect(()=>{
+        setLoader(true);
+        setTimeout(() => {
+          setLoader(false)
+        }, 1500);
+
+      },[])
+  
+  const setTheme =(color)=>{
         document.documentElement.style.setProperty('--color-bg',color)
       }
       const setColor=(event)=>{
@@ -21,8 +32,23 @@ const App = ()=>{
         localStorage.setItem('color',currentcolor);
       }
 
+
   return (
-    <>
+    <div className='App'>
+    {loader ? 
+  (
+    
+  <BeatLoader className='rotateloader'
+  style={{height:'100vh'}}
+  color={'#3189'}
+  loading={loader}
+ 
+ 
+/>
+  )
+  :(
+  
+  <>
       <Header/>
       <div className='color-list'>
       <Nav setColor={setColor}/>
@@ -33,7 +59,9 @@ const App = ()=>{
       <Portfolio/>
       <Contact/>
       <Footer/>
-    </>
+      </>
+    )}
+    </div>
   )
 
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <a href='/' className='footer_logo'>Tanmoy Chowdhury</a>
       <ul className='permalinks'>
         <li><a href='/'>Home</a></li>
-        <li><a href='#anout'>About</a></li>
+        <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#services'>Services</a></li>
         <li><a href='#portfolio'>Portfolio</a></li>
