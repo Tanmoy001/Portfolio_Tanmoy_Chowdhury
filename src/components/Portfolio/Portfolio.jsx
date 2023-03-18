0fdfe3c021bbefbacba732b65a59ccd77d906791
Tanmoy001 <chowdhurytanmoy580@gmail.com>
2023-03-19 03:25:29 +0530

    final_commit_2.1

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import './Portfolio.css'
-import img1 from '../../assets/Image1.jpeg'
 const Portfolio = () => {
   
     const data=[
       {id:1,
-      image:img1,
-      title:'title',
-      github:'https://github.com/Tanmoy001/Text-Modifier',
-      demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+      
+        title:'NEIghborING',
+        description:'where you can get to know about food attraction weather around you',
+        github:'https://github.com/Tanmoy001/Text-Modifier',
+        demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+      
     },
     {id:2,
-      image:img1,
-      title:'title',
+      title:'CoinCom',
+      description:'CoinCom will give you update about your digital coin and shares',
       github:'https://github.com/Tanmoy001/Text-Modifier',
       demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
     },
     {id:3,
-      image:img1,
-      title:'title',
+      title:'Text Modifier',
+      description:'This is a text modifier where you can edit your text',
       github:'https://github.com/Tanmoy001/Text-Modifier',
       demo:'https://635f909c1beb547f6300e782--dapper-sopapillas-8765b6.netlify.app/'
+      
     }
     ]
     return (
     <section id='portfolio'>
-      <h5>My recent work</h5>
+       <h5>My recent work</h5>
       <h2>Portfolio</h2>
-      <div className='container protfolio_container'>
+      {/* <div className='container protfolio_container'>
         { data.map(({id, image, title, github, demo})=>{
           return(
         <div key={id}className='portfolio_item'>
@@ -42,8 +44,34 @@ const Portfolio = () => {
         </div>
           )
       })}
+        </div> */}
+        <div className='portfolio_content'>
+              { data.map(({id, description, title, github, demo})=>{
+          return(
+       <div class="container">
+        <div class="drop">
+            <div class="content">
+                <h2>{id}</h2>
+         
+        <div key={id}className='portfolio_item'>
+          <div className='portfolio_item_img'>
+            
+          </div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <div className='portfolio_item-cta'>
+            <a href={github} className='btn'>Github</a>
+            <a href={demo} className='btn btn-primary'>Live Demo</a>
+          </div>
         </div>
-      
+         
+            </div>
+        </div>
+        
+    </div>
+     )
+    })}
+    </div>
     </section>
   )
 }
